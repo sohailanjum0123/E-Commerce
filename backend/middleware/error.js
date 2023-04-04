d57fa73289e5b71631d2bc8ac0d4fe16e28081da
Sohail <sohailanjum0123@gmail.com>
2023-04-04 12:49:30 +0500

    There are Fixed some Error in ErrorHandling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,7 +4,7 @@ module.exports = (err, req, res, next) =>{
     err.statusCode = err.statusCode || 500
     err.message = err.message || "Internal server Error"
 
-    res.statusCode(err.statusCode).json({
+    res.status(err.statusCode).json({
         success:false,
         message:err.message
     })
