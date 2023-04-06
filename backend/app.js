97f5a456b28d339b10a2e0b32f6e78db8830f6ca
Sohail <sohailanjum0123@gmail.com>
2023-04-06 19:48:43 +0500

    add middleware in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const dotenv = require("dotenv")
+
 const errorMiddleware = require("./middleware/error")
 
 dotenv.config({path:"backend/config/config.env"});
