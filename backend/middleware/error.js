cfacb9200552bb7155e8486c08e3afeec428d559
Sohail <sohailanjum0123@gmail.com>
2023-04-07 17:27:03 +0500

    Added cast error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,14 @@ module.exports = (err, req, res, next) =>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+    //wrong mongodb cast error
+    if(err.name==="CastError"){
+        const message = `Resource not found. Invalid ID ${err.path}`;
+
+        err =  new errorHandler(message,400)
+    }
+
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message
