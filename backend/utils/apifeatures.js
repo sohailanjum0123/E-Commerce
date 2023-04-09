99423ed48681c2af47b498ea4d78363985c08e09
Sohail <sohailanjum0123@gmail.com>
2023-04-09 12:09:27 +0500

    complete pagination filter

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -32,6 +32,9 @@ class apiFeatures{
         const currentPage = Number(this.queryStr.page) || 1;
 
         const skip =resultPerPage *  (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+        return this
     }
 }
 module.exports = apiFeatures;
