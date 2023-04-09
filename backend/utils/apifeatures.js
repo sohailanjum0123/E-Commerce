cb751eaf9e18cd71f038230320f603bbe6bcde25
Sohail <sohailanjum0123@gmail.com>
2023-04-09 12:03:24 +0500

    Paginatin filter add

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -23,9 +23,15 @@ class apiFeatures{
 
         //filter for Price and Rating
         let queryStr = JSON.stringify(queryCopy);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`)
-        this.query = this.query.find(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+        this.query = this.query.find(JSON.parse(queryStr));
         return this;
+    }
+
+    pagination(resultPerPage){
+        const currentPage = Number(this.queryStr.page) || 1;
+
+        const skip =resultPerPage *  (currentPage - 1);
     }
 }
 module.exports = apiFeatures;
